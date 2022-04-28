f3b3c56c73e35af755878b91a75d0facb63d0e76
Brandon Lum <lumjjb@gmail.com>
2022-04-28 11:22:09 -0400

    update deps for go bin
    Signed-off-by: Brandon Lum <lumjjb@gmail.com>

--- main.go
+++ main.go
@@ -3,9 +3,12 @@ package main
 import (
 	"fmt"
 
-	_ "github.com/google/uuid"
+	_ "github.com/containers/ocicrypt"
+	"github.com/google/uuid"
+	_ "github.com/kardianos/service"
 )
 
 func main() {
 	fmt.Println("hello world")
+	fmt.Println(uuid.New().String())
 }
